619f68a66f82df1bd6cab87972f98c9ffcdc8817
ayaan <ayaan@Ayaan.lan>
2023-11-10 09:39:23 -0600

    updated files

--- springboot-backend/src/main/java/com/des/backend/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/des/backend/springboot/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.des.backend.springboot.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,10 +23,10 @@ public class EmployeeController {
 	
 	// build creat employee REST API
 @PostMapping()
-public ResponseEntity<Employee> saveEmployee(Employee emp)
+public ResponseEntity<Employee> saveEmployee(@RequestBody Employee emp)
 {
 	
-	return new ResponseEntity<Employee>(emp, HttpStatus.CREATED);
+	return new ResponseEntity<Employee>(employeeService.addEmployee(emp), HttpStatus.CREATED);
 }
 
 }
