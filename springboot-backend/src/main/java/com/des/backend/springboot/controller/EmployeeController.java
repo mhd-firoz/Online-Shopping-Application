64b4b6ad0c450caa6eb224e2f1c950e3d6afa9dd
ayaan <ayaan@Ayaan.lan>
2023-11-10 09:15:31 -0600

    added controller

--- springboot-backend/src/main/java/com/des/backend/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/des/backend/springboot/controller/EmployeeController.java
@@ -1,11 +1,31 @@
 package com.des.backend.springboot.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.des.backend.springboot.model.Employee;
+import com.des.backend.springboot.service.EmployeeService;
 
 @RestController
 @RequestMapping("/tcs/employee")
 public class EmployeeController {
 	
+	private EmployeeService employeeService ;
+
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+	
+	// build creat employee REST API
+@PostMapping()
+public ResponseEntity<Employee> saveEmployee(Employee emp)
+{
+	
+	return new ResponseEntity<Employee>(emp, HttpStatus.CREATED);
+}
 
 }
